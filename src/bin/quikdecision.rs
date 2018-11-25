af1f102515f7a0aeb13f6439b9427a1478be120e
G. Wade Johnson <gwadej@anomaly.org>
2018-11-25 15:50:24 -0600

    Update to latest library

--- src/bin/quikdecision.rs
+++ src/bin/quikdecision.rs
@@ -13,7 +13,7 @@ fn main()
                 match cmd.decide()
                 {
                     Decision::Text(ans) => ans,
-                    Decision::LabeledText{ value, label } => format!("{}: \"{}\"", label, value),
+                    Decision::LabelledText{ value, label } => format!("{}: \"{}\"", label, value),
                     Decision::Num(ans) => ans.to_string(),
                     Decision::AnnotatedNum{ value, extra } => format!("{}: {}", value, extra),
                     Decision::Bool(ans) => ans.to_string(),
